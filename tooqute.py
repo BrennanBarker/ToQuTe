6c4bae62c679764895133e55ce65332afe0b13a1
Brennan Barker <BrennanBarker@users.noreply.github.com>
2020-07-29 09:53:47 -0400

    interface updates

--- tooqute.py
+++ tooqute.py
@@ -1,49 +1,54 @@
+from argparse import ArgumentParser
 import sys
-import jupyter_client
+from jupyter_client import find_connection_file, BlockingKernelClient
+
+# TODO: verbose--get additional reply fields
+# TODO: subcommmands vice choices
+# TODO: check, respond with message status
+# TODO: add better help
 
 
-def create_client_connection_to_kernel():
-    connection_file = jupyter_client.find_connection_file()
-    client = jupyter_client.BlockingKernelClient()
-    client.load_connection_file(connection_file)
+def get_kwargs():
+    arg_parser = ArgumentParser(prog='tooqute',
+            description='Interact with a Jupyter kernel from the command line')
+    arg_parser.add_argument('action', action='store',
+                            choices=['execute', 'complete', 'inspect'])
+    arg_parser.add_argument('code', action='store', nargs='?',
+                            default=sys.stdin)
+    arg_parser.add_argument('-v', '--verbose', action='store_true')
+    kwargs = vars(arg_parser.parse_args())
+
+    if hasattr(kwargs['code'], 'read'):  # If stdin or file get contents
+        kwargs['code'] = kwargs['code'].read()
+
+    return kwargs
+
+
+def client_connection_to_kernel():
+    client = BlockingKernelClient()
+    client.load_connection_file(find_connection_file())
     return client
 
 
+def requested_fields(reply, action, verbose):
+    reply_content_fields = {'execute': 'user_expressions',
+                            'complete': 'matches',
+                            'inspect': 'data'}
+    field = reply_content_fields[action]
+    return reply['content'] if verbose else reply['content'][field]
+
+
+def message_jupyter(client, action, code, verbose):
+    reply = getattr(client, action)(code, reply=True)
+    return requested_fields(reply, action, verbose)
+
+
+def main(action, code, verbose):
+    client = client_connection_to_kernel()
+    reply = message_jupyter(client, action, code, verbose)
+    print(reply if reply else 'Code sent to kernel.')
+
+
 if __name__ == '__main__':
-    
-    # Command-Line Interface
-    import argparse
-
-    arg_parser = argparse.ArgumentParser(
-            prog='tooqute',
-            description='Interact with a Jupyter kernel from the command line')
-
-    arg_parser.add_argument('code', action='store', nargs='?',
-            default=sys.stdin)
-
-    action_group = arg_parser.add_mutually_exclusive_group(required=True)
-    action_group.add_argument('-e', '--execute', action='store_true')
-    action_group.add_argument('-c', '--complete', action='store_true')
-    action_group.add_argument('-i', '--inspect', action='store_true')
-
-    args = vars(arg_parser.parse_args()) 
-
-    if type(args['code']) is not str:
-        try:
-            args['code'] = args['code'].read()
-        except AttributeError:
-            raise TypeError('Input is not a string, stdin, or file')
-
-    # Jupyter Messaging
-    client = create_client_connection_to_kernel()
-
-    if args['execute']:
-        client.execute(args['code'])
-
-    elif args['inspect']:
-        client.inspect(args['code'], reply=True)
-        print(reply['content']['data'])
-
-    elif args['complete']:
-        reply = client.complete(args['code'], reply=True)
-        print(reply['content']['matches'])
+    kwargs = get_kwargs()
+    main(**kwargs)
